678f10187d063e38262058e72dee5e50d0b9618c
tiandongxiao <team@exingdong.com>
2016-04-09 07:28:25 +0000

    支付

--- app/Http/Controllers/WxPayController.php
+++ app/Http/Controllers/WxPayController.php
@@ -8,6 +8,7 @@ use Illuminate\Http\Request;
 
 use App\Http\Requests;
 use App\Http\Controllers\Controller;
+use Illuminate\Support\Facades\Log;
 use Symfony\Component\HttpFoundation\Response;
 use EasyWeChat\Support\XML;
 
@@ -39,17 +40,10 @@ class WxPayController extends Controller
     }
     public function payCallback()
     {
+        $response = $this->payment->handleNotify(function($notify, $successful){
 
-        $response = [
-            'return_code' => 'SUCCESS',
-            'return_msg' => 'OK',
-        ];
-
-        return new Response(XML::build($response));
-
-//        $response = $this->payment->handleNotify(function($notify, $successful){
-//
-//            // 使用通知里的 "微信支付订单号" 或者 "商户订单号" 去自己的数据库找到订单
+            // 使用通知里的 "微信支付订单号" 或者 "商户订单号" 去自己的数据库找到订单
+            Log::info('This is notify transaction id --'.$notify->openid.' || '.$successful);
 //            $order = 查询订单($notify->transaction_id);
 //
 //            if (!$order) { // 如果订单不存在
@@ -74,9 +68,45 @@ class WxPayController extends Controller
 //            $order->save(); // 保存订单
 //
 //            return true; // 返回处理完成
-//        });
+        });
+
+        return $response;
+    }
+
+
+    public function orderCallback()
+    {
+        Log::info('This is orderCallback function');
+        $response = $this->payment->handleNotify(function($notify, $successful){
+
+            // 使用通知里的 "微信支付订单号" 或者 "商户订单号" 去自己的数据库找到订单
+            Log::info('This is notify transaction id --'.$notify->transaction_id);
+//            $order = 查询订单($notify->transaction_id);
 //
-//        return $response;
+//            if (!$order) { // 如果订单不存在
+//                return 'Order not exist.'; // 告诉微信，我已经处理完了，订单没找到，别再通知我了
+//            }
+//
+//            // 如果订单存在
+//            // 检查订单是否已经更新过支付状态
+//            if ($order->paid_at) { // 假设订单字段“支付时间”不为空代表已经支付
+//                return true; // 已经支付成功了就不再更新了
+//            }
+//
+//            // 用户是否支付成功
+//            if ($successful) {
+//                // 不是已经支付状态则修改为已经支付状态
+//                $order->paid_at = time(); // 更新支付时间为当前时间
+//                $order->status = 'paid';
+//            } else { // 用户支付失败
+//                $order->status = 'paid_fail';
+//            }
+//
+//            $order->save(); // 保存订单
+//
+//            return true; // 返回处理完成
+        });
+
     }
 
     public function newOrder()
