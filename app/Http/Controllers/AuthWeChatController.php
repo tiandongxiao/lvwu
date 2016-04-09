d07235f12719d79d8aa9a775c88a05f550e91c9b
tiandongxiao <team@exingdong.com>
2016-04-09 13:36:43 +0000

    支付

--- app/Http/Controllers/AuthWeChatController.php
+++ app/Http/Controllers/AuthWeChatController.php
@@ -121,5 +121,19 @@ class AuthWeChatController extends Controller
                 return redirect('/')->withErrors('您的信息已被记录，恶意攻击将被记录在案');
         }
     }
+
+    public function wxCheck()
+    {
+        Log::info('request arrived.'); # 注意：Log 为 Laravel 组件，所以它记的日志去 Laravel 日志看，而不是 EasyWeChat 日志
+
+        $wechat = app('wechat');
+        $wechat->server->setMessageHandler(function($message){
+            return "欢迎关注 overtrue！";
+        });
+
+        Log::info('return response.');
+
+        return $wechat->server->serve();
+    }
 }
 
