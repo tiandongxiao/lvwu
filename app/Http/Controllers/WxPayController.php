32d45840a692847f6718d897171f03bc3b39f72c
tiandongxiao <team@exingdong.com>
2016-04-09 06:02:23 +0000

    支付

--- app/Http/Controllers/WxPayController.php
+++ app/Http/Controllers/WxPayController.php
@@ -37,7 +37,39 @@ class WxPayController extends Controller
     }
     public function payCallback()
     {
-        dd('I am callback');
+        //dd('I am callback');
+        $result = $this->payment->prepare($order);
+        $prepayId = $result->prepay_id;
+
+        $response = $this->payment->handleNotify(function($notify, $successful){
+            // 使用通知里的 "微信支付订单号" 或者 "商户订单号" 去自己的数据库找到订单
+            $order = 查询订单($notify->transaction_id);
+
+            if (!$order) { // 如果订单不存在
+                return 'Order not exist.'; // 告诉微信，我已经处理完了，订单没找到，别再通知我了
+            }
+
+            // 如果订单存在
+            // 检查订单是否已经更新过支付状态
+            if ($order->paid_at) { // 假设订单字段“支付时间”不为空代表已经支付
+                return true; // 已经支付成功了就不再更新了
+            }
+
+            // 用户是否支付成功
+            if ($successful) {
+                // 不是已经支付状态则修改为已经支付状态
+                $order->paid_at = time(); // 更新支付时间为当前时间
+                $order->status = 'paid';
+            } else { // 用户支付失败
+                $order->status = 'paid_fail';
+            }
+
+            $order->save(); // 保存订单
+
+            return true; // 返回处理完成
+        });
+
+        return $response;
     }
 
     public function newOrder()
@@ -52,14 +84,6 @@ class WxPayController extends Controller
         ]);
         $result = $this->payment->prepare($order);
         $prepayId = $result->prepay_id;
-
-
-        $json = $this->payment->configForPayment($prepayId,false);
-
-
-
-
-        dd($json);
     }
 
     public function generateQrCode()
@@ -71,6 +95,8 @@ class WxPayController extends Controller
     {
         $product_id = 'thisisanewgood';
         $price = 1;
+
+
         $url = $this->payment->scheme('goodhappy');
         return view('payment.good',compact('url','price'));
     }
@@ -78,7 +104,19 @@ class WxPayController extends Controller
     public function payTest($product_id)
     {
         $price = random_int(1,10);
+        $order = new Order([
+            'body'             => 'iPad mini 16G 白色',
+            'detail'           => 'iPad mini 16G 白色',
+            'out_trade_no'     => '1217752501201407033233368018',
+            'total_fee'        => random_int(1,10),
+            'notify_url'       => 'http://www.exingdong.com/wxpay/callback',
+            'trade_type'        =>  'NATIVE'
+        ]);
+
+        $result = $this->payment->prepare($order);
+        $prepayId = $result->prepay_id;
         $url = $this->payment->scheme($product_id);
+
         return view('payment.good',compact('url','price'));
     }
 }
--- resources/views/payment/good.blade.php
+++ resources/views/payment/good.blade.php
@@ -2,9 +2,7 @@
 @section('content')
     <div class="text-center">
         <h3>{{$price/100}} 元</h3>
-        {!! QrCode::encoding('UTF-8')->size(400)->generate("weixin：//wxpay/bizpayurl?appid=wx2421b1c4370ec43b&mch_id=10000100&nonce_str=f6808210402125e30663234f94c87a8c&product_id=1&time_stamp=1415949957&sign=512F68131DD251DA4A45DA79CC7EFE9D
-
-") !!}
+        {!! QrCode::encoding('UTF-8')->size(400)->generate($url) !!}
         <p>请使用微信扫码支付</p>
     </div>
     @endsection
