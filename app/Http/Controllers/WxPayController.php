6debf7acf9dc38f9126690882003c4254a2faafc
tiandongxiao <team@exingdong.com>
2016-04-10 06:11:10 +0000

    微信

--- app/Http/Controllers/WxPayController.php
+++ app/Http/Controllers/WxPayController.php
@@ -16,27 +16,12 @@ use EasyWeChat\Support\XML;
 
 class WxPayController extends Controller
 {
-    private $options = [
-        // 前面的appid什么的也得保留哦
-        'app_id' => 'wxece8375442c7704d',
+    private $app;
+    private $payment;
 
-        // payment
-        'payment' => [
-            'merchant_id'        => '1270537701',
-            'key'                => 'sanmingzhi19811121tina19850811ov',
-            'cert_path'          => 'http://www.exingdong.com/cer/wx/apiclient_cert.pem', // XXX: 绝对路径！！！！
-            'key_path'           => 'http://www.exingdong.com/cer/wx/apiclient_key.pem',      // XXX: 绝对路径！！！！
-            'notify_url'         => 'http://www.exingdong.com/wxpay/callback',       // 你也可以在下单时单独设置来想覆盖它
-        ],
-    ];
-
-    private $app = null;
-    private $payment = null;
-
-
-    public function __construct()
+    public function __construct(Application $application)
     {
-        $this->app = new Application($this->options);
+        $this->app = $application;
         $this->payment = $this->app->payment;
 
     }
